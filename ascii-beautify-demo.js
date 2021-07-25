1a1d931d61afb81c96e6e6aac13dc8c06c697547
Kasey <kaseyjameshinton@gmail.com>
2021-07-25 00:46:16 -0600

    A few small fixes

--- ascii-beautify-demo.js
+++ ascii-beautify-demo.js
@@ -62,7 +62,11 @@ class AsciiBeautifyDemo extends LitElement {
   }
 
   async updated(changedProps) {
-    if (changedProps.has("ascii") || changedProps.has("selectedTheme") || changedProps.has("selectedDesign")) {
+    if (
+      changedProps.has("ascii") ||
+      changedProps.has("selectedTheme") ||
+      changedProps.has("selectedDesign")
+    ) {
       if (!!this.selectedTheme && !!this.ascii && !!this.selectedDesign) {
         this.subTheme =
           asciiBeautifyReduce(this.selectedTheme, this.ascii) ?? {};
@@ -188,21 +192,13 @@ class AsciiBeautifyDemo extends LitElement {
       }
 
       button-container {
-          display: flex;
+        display: flex;
       }
 
       mwc-button {
         align-self: flex-start;
         margin-top: 24px;
         margin-right: 12px;
-        display: flex;
-        align-item: center;
-      }
-
-      mwc-button svg {
-          width: 18px;
-          height: 18px;
-          margin-right: 12px;
       }
 
       [hidden] {
@@ -240,7 +236,9 @@ class AsciiBeautifyDemo extends LitElement {
           this.selectedDesign = this.designs.find(
             (design) => design.name === e.target.value
           );
-          this.selectedTheme = this.themes?.find(theme => theme.name === this.selectedTheme?.name);
+          this.selectedTheme = this.themes?.find(
+            (theme) => theme.name === this.selectedTheme?.name
+          );
           this.ascii = fillBackgroundSpaces(this.selectedDesign.ascii);
         }}
       >
@@ -288,7 +286,7 @@ class AsciiBeautifyDemo extends LitElement {
                 .value=${a[1]}
                 @change=${(e) => {
                   this.subTheme.colors[a[0]] = e.target.value;
-                  this.selectedTheme = {...this.subTheme};
+                  this.selectedTheme = { ...this.subTheme };
                 }}
                 type="color"
               />
@@ -302,74 +300,79 @@ class AsciiBeautifyDemo extends LitElement {
         .colors=${this.selectedTheme?.colors ?? {}}
       ></ascii-beautify>
       <button-container>
-      <mwc-button
-        @click=${() => {
-          const asciiBeautify = this.shadowRoot.querySelector("ascii-beautify");
-          asciiBeautify.downloadImage();
-
-        }}
-        ?hidden=${!this.ascii || !this.selectedTheme}
-        raised
-        ><svg viewBox="0 0 24 24">
-    <path fill="currentColor" d="M8,13H10.55V10H13.45V13H16L12,17L8,13M19.35,10.04C21.95,10.22 24,12.36 24,15A5,5 0 0,1 19,20H6A6,6 0 0,1 0,14C0,10.91 2.34,8.36 5.35,8.04C6.6,5.64 9.11,4 12,4C15.64,4 18.67,6.59 19.35,10.04M19,18A3,3 0 0,0 22,15C22,13.45 20.78,12.14 19.22,12.04L17.69,11.93L17.39,10.43C16.88,7.86 14.62,6 12,6C9.94,6 8.08,7.14 7.13,8.97L6.63,9.92L5.56,10.03C3.53,10.24 2,11.95 2,14A4,4 0 0,0 6,18H19Z" />
-</svg> PNG</mwc-button
-      >
-      <mwc-button
-        @click=${() => {
-          this.gen_xpm(8);
-          
-        }}
-        ?hidden=${!this.ascii || !this.selectedTheme}
-        outlined
-        ><svg viewBox="0 0 24 24">
-    <path fill="currentColor" d="M8,13H10.55V10H13.45V13H16L12,17L8,13M19.35,10.04C21.95,10.22 24,12.36 24,15A5,5 0 0,1 19,20H6A6,6 0 0,1 0,14C0,10.91 2.34,8.36 5.35,8.04C6.6,5.64 9.11,4 12,4C15.64,4 18.67,6.59 19.35,10.04M19,18A3,3 0 0,0 22,15C22,13.45 20.78,12.14 19.22,12.04L17.69,11.93L17.39,10.43C16.88,7.86 14.62,6 12,6C9.94,6 8.08,7.14 7.13,8.97L6.63,9.92L5.56,10.03C3.53,10.24 2,11.95 2,14A4,4 0 0,0 6,18H19Z" />
-</svg> XPM</mwc-button
-      >
-     </button-container>
+        <mwc-button
+          @click=${() => {
+            const asciiBeautify =
+              this.shadowRoot.querySelector("ascii-beautify");
+            asciiBeautify.downloadImage();
+          }}
+          ?hidden=${!this.ascii || !this.selectedTheme}
+          raised
+          title="Download PNG File"
+          icon="image"
+          >
+          PNG</mwc-button
+        >
+        <mwc-button
+          @click=${() => {
+            this.gen_xpm(8);
+          }}
+          ?hidden=${!this.ascii || !this.selectedTheme}
+          outlined
+          title="Download XPM File"
+          icon="sim_card_download"
+        >
+          XPM</mwc-button
+        >
+      </button-container>
     `;
   }
-  gen_xpm(scale=1) {
-    scale=1;//scale has to be 1 for xpm file output
-    var xpm="";
-    var header=`/* XPM */
+  gen_xpm(scale = 1) {
+    scale = 1; //scale has to be 1 for xpm file output
+    var xpm = "";
+    var header = `/* XPM */
 static char * xpm[] = {
 `;
-    var keys=Object.keys(this.subTheme.colors);
-    var w = this.ascii.replace("\r","").split("\n")[0].length;
+    var keys = Object.keys(this.subTheme.colors);
+    var w = this.ascii.replace("\r", "").split("\n")[0].length;
     var h = this.ascii.split("\n").length - 1;
     var c = keys.length;
     xpm += header;
-    var sp=" ";
-    xpm += "\"" + w + sp + h + sp + c + sp + scale + "\",";
-    for (var i=0; i<keys.length; i++) {
+    var sp = " ";
+    xpm += '"' + w + sp + h + sp + c + sp + scale + '",';
+    for (var i = 0; i < keys.length; i++) {
       var key = keys[i];
       var val = key == "sp" ? " " : key;
       xpm += `
 `;
-      xpm += "\"" + val + sp + "c" + sp + this.subTheme.colors[key] + "\",";
+      xpm += '"' + val + sp + "c" + sp + this.subTheme.colors[key] + '",';
     }
     xpm += `
 "`;
-    for (var i=0; i<this.ascii.length; i++) {
+    for (var i = 0; i < this.ascii.length; i++) {
       var a = this.ascii[i];
-      if (a == '\r'){continue;}
-      if (a == '\n'){
-	xpm += '",';
-	xpm += `
-"`;
+      if (a == "\r") {
+        continue;
       }
-      else {
-	xpm += a;
+      if (a == "\n") {
+        xpm += '",';
+        xpm += `
+"`;
+      } else {
+        xpm += a;
       }
     }
-    xpm = xpm.substring(0,xpm.length-3);
+    xpm = xpm.substring(0, xpm.length - 3);
     xpm += `
 `;
-    xpm += '};';
-    var el = document.createElement('a');
-    el.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(xpm));
-    el.setAttribute('download', 'img.xpm');
-    el.style.display = 'none';
+    xpm += "};";
+    var el = document.createElement("a");
+    el.setAttribute(
+      "href",
+      "data:text/plain;charset=utf-8," + encodeURIComponent(xpm)
+    );
+    el.setAttribute("download", "img.xpm");
+    el.style.display = "none";
     document.body.appendChild(el);
     el.click();
     document.body.removeChild(el);
